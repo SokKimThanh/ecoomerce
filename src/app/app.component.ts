ad4d3267cdf224f15eac291467d8251e4256a077
Sok Kim Thanh <thanhsk1991@gmail.com>
2020-11-06 16:16:47 +0700

    update tab active

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+export interface Link {
+  id: number;
+  name: string;
+  url: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ecoomerce';
+export class AppComponent implements OnInit {
+  links: Link[];
+  tab: number;
+  activeLink(tab): void {
+    if (tab === 1) {
+      this.tab = 1;
+    } else if (tab === 2) {
+      this.tab = 2;
+    } else if (tab === 3) {
+      this.tab = 3;
+    }
+  }
+  ngOnInit(): void {
+    this.tab = 1;
+  }
 }
+
